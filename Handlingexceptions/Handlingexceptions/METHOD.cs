081bcfe4ba404a2cfad2a0674d600e3deb94b0ee
perfecta1000 <oyikwuesther@gmail.com>
2022-07-08 14:00:07 +0100

    method

---
+++ Handlingexceptions/Handlingexceptions/METHOD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handlingexceptions
+{
+    internal class METHOD
+    {
+        public string MethodName { get; set; }
+        public string MethodBody { get; set; }
+        public int MethodCount { get; set; }
+
+        public METHOD()
+        { 
+        
+        }
+    }
+}
